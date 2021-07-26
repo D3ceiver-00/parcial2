8145496eb6cae48cb8dad785fe577d0c377b2922
D3ceiver <A01207532@itesm.mx>
2021-07-26 18:18:29 -0500

    examen listo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ## Agrega la dependencia de express ##
-
+var express = require("express");
 
 var animals = [
    {
@@ -26,13 +26,14 @@ var animals = [
 
 
 // ## Inicializa express ##
-
+var app = express();
 
 // ## Inicializa el motor de plantillas con EJS ##
-
+app.set('view engine', 'ejs')
 
 // ## Agrega el middleware de express para que el servidor soporte json ##
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 /* ############## RUTAS ################  */
 
@@ -43,21 +44,37 @@ var animals = [
       b) animals: con referencia al arreglo animals. 
 */
 
+app.get("/all-pets", (req,res)=>{
+  //res.send("all-animals");
+  res.render("pages/all-pets",{title:"All",animals:animals});
+})
 
 /* (2)  Crea una ruta POST que: 
    - escuche en /api/addAnimal 
    - obtenga el valor recibido del body
    - lo agregue al arreglo animals
-
 */
- 
+app.post("/api/addAnimal", (req,res)=>{
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newAnimal = req.body;
+  //newAnimal.animalType = newAnimal.any.replace(/\s+/g, "").toLowerCase();
+  console.log(newAnimal);
+
+  animals.push(newAnimal);
+  res.json(newAnimal);  
+})
+
 /* (3)  Crea una ruta GET que: 
    - escuche en /dog  
    - renderice la página 'pages/dog' y reciba 1 objeto con 2 propiedades: 
       a) title:  con el valor "Dog" 
       b) animals: con el valor del indice[0]
 */ 
-
+app.get("/dog", (req,res)=>{
+  //res.send(animals[0])
+  res.render("pages/dog", {title:"Dog", dog: animals[0]})
+})
 
 /* (4)  Crea una ruta GET que: 
    - escuche en /api/getAnimal/:animal
@@ -68,9 +85,21 @@ var animals = [
       a) title:  con el valor obtenido de la ruta dinámica
       b) animal: con la variable que almacena el objeto encontrado. Si no lo encuentra la variable se va vacía
 */ 
-   
+app.get("/api/getAnimal/:animal", (req,res)=>{
+  var animal = req.params.animal;
+  console.log(animal);
+  for (var i = 0; i < animals.length; i++) {
+    if (animal === animals[i].animalType) {
+      return res.json(animals[i]);
+    }
+  }
+
+  return res.json("no animal found");
+})   
 
 
 //  Agrega el código necesario para que el servidor escuche en el puerto 5000
-
-
+var PORT= process.env.PORT || 5000
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+})
